20efba932095eb4fc3f7085a6fdbccbb47d4fb08
Rintaro Okamura <rintaro.okamura@gmail.com>
2019-02-28 22:20:31 +0900

    Add open, options func

--- src/clj_halodb/core.clj
+++ src/clj_halodb/core.clj
@@ -2,4 +2,30 @@
   (:require [clojure.spec.alpha :as spec])
   (:import [com.oath.halodb HaloDB HaloDBOptions]))
 
+(defn ^HaloDBOptions options
+  "Returns the HaloDBOptions instance."
+  [opts]
+  (HaloDBOptions.))
 
+(defn ^HaloDB open
+  "Returns the HaloDB instance."
+  [^String directory ^HaloDBOptions opts]
+  (HaloDB/open directory opts))
+
+(comment
+  (def directory "directory")
+  (def halodb-options (options {}))
+
+  (def halodb
+    (open directory halodb-options))
+
+  (-> halodb
+      (.put (.getBytes "a") (.getBytes "value of a")))
+
+  (-> halodb
+      (.get (.getBytes "a"))
+      (String.))
+
+  (-> halodb
+      (.close))
+  )
